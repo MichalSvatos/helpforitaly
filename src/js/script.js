41d2d4940f387a9bf275e53fa53285a94b85fe89
Michal Svatoš <michal.svatos@gmail.com>
2020-03-15 23:56:43 +0100

    New structure + institution update

--- src/js/script.js
+++ src/js/script.js
@@ -24,21 +24,55 @@ function ajax_get(url, callback) {
 }
 
 ajax_get('data/institutions.json', function(data) {
+	let locations = [];
 	let html = '';
+	let finalHtml = '';
 
-	for (let i=0; i < data['institutions'].length; i++) {
-		html += `<a href="${data['institutions'][i]["url"]}" target="_blank" rel="noopener" class="institution__item">
+	// ... with the help from Petr Kinc :)
+	for (let i = 0; i < data['institutions'].length; i++) {
+
+		let location = data['institutions'][i]["location"];
+		let hasLocationGroup = locations.find(o => o.name === location);
+		// let payments = data['institutions'][i]["payment"];
+
+		let html =  `<a href="${data['institutions'][i]["url"]}" target="_blank" rel="noopener" class="institution__item">
 					<span class="institution__title">${data['institutions'][i]["name"]}</span>
 					<span class="institution__url">${data['institutions'][i]["url"]}</span>
 					<span class="institution__desc">${data['institutions'][i]["desc"]}</span>
-					<span class="institution__loc">
-						<span class="loc">${data['institutions'][i]["location"]}</span>
+					<span class="institution__pay">
+						<span class="payment"></span>
 					</span>
 				</a>`;
+
+		if (hasLocationGroup) {
+			let index = locations.findIndex(x => x.name === location);
+			locations[index].html += html;
+
+		} else {
+			let locationObj = {
+				name : location,
+				html : html
+			};
+
+			locations.push(locationObj);
+		}
 	}
 
-	document.getElementById("institutions").innerHTML = html;
+	for (let i = 0; i < locations.length; i++) {
+		let name = locations[i].name;
+		let html = locations[i].html;
+
+		finalHtml += `<div class="instutitions__section">
+						<h4 class="institutions__region-title">${name}</h4>
+
+						<div class="institution__region-wrapper">${html}</div>
+					</div>`;
+	}
+
+	document.getElementById("institutions").innerHTML = finalHtml;
+
 });
+
 
 ajax_get('data/links.json', function (data) {
 	let links = '';
